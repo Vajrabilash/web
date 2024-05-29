f5c9421d6f351a807c7841ca568efc3a9188dcc4
vajrabilash <degapoguvajrabilash@gmail.com>
2024-05-29 21:31:11 +0530

    basic web

---
+++ basic/scrp.js
@@ -0,0 +1,8 @@
+
+function sb(){
+    var v1 = parseInt(document.getElementById('fr').value);
+    var v2 = parseInt(document.getElementById('sc').value);
+    var ans = v1+v2;
+    document.getElementById('an').value = ans;
+    console.log(ans);
+}
